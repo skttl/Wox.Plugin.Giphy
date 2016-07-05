6edbbc47c31b9d8b421ee841817793217f9a5aa2
skttl <sk@ecreo.dk>
2016-07-05 20:51:23 +0200

    Add project files.

---
+++ Wox.Plugin.Giphy/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wox.Plugin.Giphy
+{
+    public class Class1
+    {
+    }
+}
